7e44feaf6d41d17f9f2d457cd4cd2fa2333649bb
Lina <44583511+LinaRise@users.noreply.github.com>
2021-07-26 20:42:25 +0300

    Initial commit

---
+++ app/src/main/java/com/example/android/andersen_flags/FlagActivity.kt
@@ -0,0 +1,11 @@
+package com.example.android.andersen_flags
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FlagActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
